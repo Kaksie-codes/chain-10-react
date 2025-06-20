4ea1387037b013cee2678afc31bdcd7dbb6e8ec8
Nsikak Akpan <builtbykaksy@gmail.com>
2025-06-20 17:12:53 +0100

    Update Animal.jsx

--- vite-project/src/components/Animal.jsx
+++ vite-project/src/components/Animal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Animal = () => {
     const _animals = ["sheep", "goat", "pig", "frog", "Zebra"];
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const [animals, setAnimals] = useState(_animals)
     const [newAnimal, setNewAnimal] = useState('');
 
@@ -30,6 +31,13 @@ const Animal = () => {
                 animals.map((amimal, i) => (
                     <li key={i}>{amimal}</li>
                 ))
+            }
+        </ul>
+        <ul>
+            {
+                numbers.filter((num) => num % 2 === 0).map((num, i) => (
+                    <li key={i}>{num}</li>
+                ) )
             }
         </ul>
     </div>
